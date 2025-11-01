4d80d2ec2d7d4d14a2f11a0ca97326fecbc52991
Jörg Thalheim <joerg@thalheim.io>
2025-11-01 21:12:30 +0100

    add package

--- flake.nix
+++ flake.nix
@@ -20,6 +20,36 @@
         system,
         ...
       }: {
+        packages.default = pkgs.buildNpmPackage {
+          pname = "n8n-nodes-caldav";
+          version = "1.0.0";
+
+          src = ./.;
+
+          npmDepsHash = "sha256-wxr1/O0k4PRSHOuhmgoE3bJy4aiVf8FJl2uSxF2Gca8=";
+
+          makeCacheWritable = true;
+          npmFlags = ["--ignore-scripts"];
+
+          buildPhase = ''
+            runHook preBuild
+            npm run build
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/lib/node_modules/n8n-nodes-caldav
+            cp -r dist package.json node_modules $out/lib/node_modules/n8n-nodes-caldav/
+            runHook postInstall
+          '';
+
+          meta = {
+            description = "n8n node for CalDAV integration";
+            license = pkgs.lib.licenses.mit;
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs
